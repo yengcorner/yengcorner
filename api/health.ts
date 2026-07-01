f8dda1071089a40404d9be658a18397b7d3cb71e
YENG CORNER <taphoayeng12@gmail.com>
2026-07-02 00:10:11 +0700

    Update health.ts

--- api/health.ts
+++ api/health.ts
@@ -1,10 +1,5 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default function handler(
-  req: VercelRequest,
-  res: VercelResponse
-) {
+export default function handler(req: any, res: any) {
   res.status(200).json({
-    status: "ok"
+    status: "ok",
   });
 }
