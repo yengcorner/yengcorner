dd5888091a22ad3ddd198d313b345fc1c0969165
YENG CORNER <taphoayeng12@gmail.com>
2026-07-01 23:55:33 +0700

    Update firebase.ts

--- api/firebase.ts
+++ api/firebase.ts
@@ -1,1 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc } from "firebase/firestore";
+import firebaseConfig from "../firebase-applet-config.json";
 
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const db = getFirestore(
+  firebaseApp,
+  firebaseConfig.firestoreDatabaseId
+);
+
+export const gmailDocRef = doc(
+  db,
+  "gmail",
+  "config_YengCornerSecret_3bf8d79a29e4"
+);
