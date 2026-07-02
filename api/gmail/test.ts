0e59cd6d55cd5574ebe02bdbd309c06ad7cc07be
YENG CORNER <taphoayeng12@gmail.com>
2026-07-02 14:56:15 +0700

    Modify nodemailer transport settings in test.ts
    Updated nodemailer transport configuration to set secure to false.

--- api/gmail/test.ts
+++ api/gmail/test.ts
@@ -6,7 +6,7 @@ export default async function handler(req: Request, res: Response) {
   const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
     port: 587,
-   // Tạm thời sửa chỗ này trong file test.ts
+    secure: false,
 auth: {
   user: 'taphoayeng12@gmail.com',
   pass: process.env.SMTP_PASS,
