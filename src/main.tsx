bf0222a0f291476e69e5043ba142c7bd3236bddc
YENG CORNER <taphoayeng12@gmail.com>
2026-07-04 12:57:43 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <Analytics />
   </StrictMode>,
 );
